2f5fb830882bb7fad804fffeae4e8f232b741760
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-01-20 22:41:15 -0700

    refactor: :coffin: Remove comment from previous implementation

--- api/index.js
+++ api/index.js
@@ -15,12 +15,3 @@ app.get('/', (context) => {
 app.get('/leaderboard', (context) => context.json(leaderboard))
 
 export default app
-/* export default {
-	async fetch(request, env, ctx) {
-		return new Response(JSON.stringify(leaderboard), {
-			headers: {
-				'content-type': 'application/json;charset=UTF-8'
-			}
-		});
-	},
-}; */
