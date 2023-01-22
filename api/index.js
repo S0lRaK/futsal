b8935f1028921f2349c77c20d7f0cc177e03948f
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-01-21 19:39:29 -0700

    feat(api): :rocket: Serve static assets with Hono

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
 
 const app = new Hono()
 
-app.get('/', (context) => {
-	return context.json([
-		{
-			endpoint: '/leaderboard',
-			description: 'Returns the leaderboard'
-		}
-	])
-})
+app.get('/', (context) => context.json([
+	{
+		endpoint: '/leaderboard',
+		description: 'Returns the leaderboard'
+	}
+]))
 
 app.get('/leaderboard', (context) => context.json(leaderboard))
+
+app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
