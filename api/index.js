12d05bffc67672475ca00eada5dd19ee36ed630e
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-01-24 21:48:42 -0700

    feat(api): :sparkles: Add endpoint for teams

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leaderboard from '../db/leaderboard.json'
+import teams from '../db/teams.json'
 
 const app = new Hono()
 
@@ -8,10 +9,16 @@ app.get('/', (context) => context.json([
 	{
 		endpoint: '/leaderboard',
 		description: 'Returns the leaderboard'
+	},
+	{
+		endpoint: '/teams',
+		description: 'Returns the teams'
 	}
 ]))
 
 app.get('/leaderboard', (context) => context.json(leaderboard))
+
+app.get('/teams', (context) => context.json(teams))
 
 app.get('/static/*', serveStatic({ root: './' }))
 
