6f02f28cadc6431d60a875c3958e0bc657872780
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-01-28 17:32:43 -0700

    feat(api): :sparkles: Add endpoint for team by id

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
+
 import leaderboard from '../db/leaderboard.json'
 import teams from '../db/teams.json'
 
@@ -19,6 +20,15 @@ app.get('/', (context) => context.json([
 app.get('/leaderboard', (context) => context.json(leaderboard))
 
 app.get('/teams', (context) => context.json(teams))
+
+app.get('/teams/:id', (context) => {
+	const teamId = context.req.param('id')
+	const teamFoundById = teams.find((team) => team.id === teamId)
+
+	return teamFoundById
+		? context.json(teamFoundById)
+		: context.json({ message: 'Team not found' }, 404)
+})
 
 app.get('/static/*', serveStatic({ root: './' }))
 
