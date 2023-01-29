b40dc39b995a4dcebc4b5a75fb2b1dbc2e37cdd1
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-01-29 16:23:01 -0700

    test(test): :white_check_mark: Set vitest and check for routes
    Test if routes have an endpoint and a description

--- api/index.test.js
+++ api/index.test.js
@@ -1,24 +1,27 @@
-import { unstable_dev } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev } from 'wrangler'
+import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 
-describe("Worker", () => {
-	let worker;
+describe('Worker', () => {
+	let worker
 
 	beforeAll(async () => {
-		worker = await unstable_dev("src/index.js", {
+		worker = await unstable_dev('api/index.js', {
 			experimental: { disableExperimentalWarning: true },
-		});
-	});
+		})
+	})
 
 	afterAll(async () => {
-		await worker.stop();
-	});
+		await worker.stop()
+	})
 
-	it("should return Hello World", async () => {
-		const resp = await worker.fetch();
-		if (resp) {
-			const text = await resp.text();
-			expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+	it('routes should have an endpoint and a description', async () => {
+		const response = await worker.fetch()
+		if (response) {
+			const apiRoutes = await response.json()
+			apiRoutes.forEach(route => {
+				expect(route).toHaveProperty('endpoint')
+				expect(route).toHaveProperty('description')
+			})
 		}
-	});
-});
+	})
+})
