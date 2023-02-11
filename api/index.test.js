82e4ce31040c56dae1dd6dfee442068a5b8057fa
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-02-11 13:31:16 -0700

    test(test): :white_check_mark: Add tests for /teams route
    Test number of teams, properties of teams, and 404 error message for non-existing teams

--- api/index.test.js
+++ api/index.test.js
@@ -1,12 +1,12 @@
 import { unstable_dev } from 'wrangler'
 import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 
-describe('Worker', () => {
+describe('Testing / route', () => {
 	let worker
 
 	beforeAll(async () => {
 		worker = await unstable_dev('api/index.js', {
-			experimental: { disableExperimentalWarning: true },
+			experimental: { disableExperimentalWarning: true }
 		})
 	})
 
@@ -14,13 +14,65 @@ describe('Worker', () => {
 		await worker.stop()
 	})
 
-	it('routes should have an endpoint and a description', async () => {
+	it('Routes should have an endpoint and a description', async () => {
 		const response = await worker.fetch()
 		if (response) {
 			const apiRoutes = await response.json()
 			apiRoutes.forEach(route => {
 				expect(route).toHaveProperty('endpoint')
 				expect(route).toHaveProperty('description')
+			})
+		}
+	})
+})
+
+describe('Testing /teams route', () => {
+	let worker
+
+	beforeAll(async () => {
+		worker = await unstable_dev('api/index.js', {
+			experimental: { disableExperimentalWarning: true }
+		})
+	})
+
+	afterAll(async () => {
+		await worker.stop()
+	})
+
+	it('Teams endpoint should have all teams', async() => {
+		const response = await worker.fetch('/teams')
+		if (response) {
+			const teams = await response.json()
+			const numberOfTeams = Object.entries(teams).length
+			expect(numberOfTeams).toBe(16)
+		}
+	})
+
+	it('Teams should have all props', async() => {
+		const response = await worker.fetch('/teams')
+		if (response) {
+			const teams = await response.json()
+
+			teams.forEach((team) => {
+				expect(team).toHaveProperty('id')
+				expect(team).toHaveProperty('name')
+				expect(team).toHaveProperty('web')
+				expect(team).toHaveProperty('est')
+				expect(team).toHaveProperty('president')
+				expect(team).toHaveProperty('stadium')
+				expect(team).toHaveProperty('coach')
+				expect(team).toHaveProperty('players')
+				expect(team).toHaveProperty('social')
+			})
+		}
+	})
+
+	it('Non existing tema should return "not found" message', async() => {
+		const response = await worker.fetch('/teams/noexist')
+		if (response) {
+			const errorMessage = await response.json()
+			expect(errorMessage).toEqual({
+				message: 'Team not found'
 			})
 		}
 	})
