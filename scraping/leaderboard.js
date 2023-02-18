61cbb208a8d1d560f575d5def784ea42e1902930
Carlos J. García Carmona <carlos@carlosjgc.me>
2023-02-18 16:50:43 -0700

    refactor(scraping): :recycle: Extract scraping logic

--- scraping/leaderboard.js
+++ scraping/leaderboard.js
@@ -1,23 +1,20 @@
-import { writeDBFile } from '../db/index.js'
-import { URLS, cleanText, scrape } from './utils.js'
-import { logError, logInfo, logSuccess } from './log.js'
+import { cleanText } from './utils.js'
 import TEAMS from '../db/teams.json' assert { type: 'json'}
 
-async function getLeaderboard () {
-	const $ = await scrape(URLS.leaderboard)
-	const $rows = $('table tbody tr')
+const LEADERBOARD_SELECTORS = {
+	team: { selector: '> :nth-child(3)', typeOf: 'string' },
+	points: { selector: '> :nth-child(4)', typeOf: 'number' },
+	gamesPlayed: { selector: '> :nth-child(5)', typeOf: 'number' },
+	gamesWon: { selector: '> :nth-child(6)', typeOf: 'number' },
+	gamesDrawn: { selector: '> :nth-child(7)', typeOf: 'number' },
+	gamesLost: { selector: '> :nth-child(8)', typeOf: 'number' },
+	goalsFor: { selector: '> :nth-child(9)', typeOf: 'number' },
+	goalsAgainst: { selector: '> :nth-child(10)', typeOf: 'number' },
+	goalsDifference: { selector: '> :nth-child(11)', typeOf: 'number' }
+}
 
-	const LEADERBOARD_SELECTORS = {
-		team: { selector: '> :nth-child(3)', typeOf: 'string' },
-		points: { selector: '> :nth-child(4)', typeOf: 'number' },
-		gamesPlayed: { selector: '> :nth-child(5)', typeOf: 'number' },
-		gamesWon: { selector: '> :nth-child(6)', typeOf: 'number' },
-		gamesDrawn: { selector: '> :nth-child(7)', typeOf: 'number' },
-		gamesLost: { selector: '> :nth-child(8)', typeOf: 'number' },
-		goalsFor: { selector: '> :nth-child(9)', typeOf: 'number' },
-		goalsAgainst: { selector: '> :nth-child(10)', typeOf: 'number' },
-		goalsDifference: { selector: '> :nth-child(11)', typeOf: 'number' }
-	}
+export async function getLeaderboard ($) {
+	const $rows = $('table tbody tr')
 
 	const getTeam = ({ name }) => TEAMS.find(team => team.name === name)
 
@@ -46,17 +43,4 @@ async function getLeaderboard () {
 		})
 	})
 	return leaderboard
-}
-
-try {
-	logInfo('Scraping leaderboard...')
-	const leaderboard = await getLeaderboard()
-	logSuccess('Leaderboard scraped successfully')
-
-	logInfo('Writing leaderboard to database...')
-	await writeDBFile('leaderboard', leaderboard)
-	logSuccess('Leaderboard written to database successfully')
-} catch (error) {
-	logError('Error scraping leaderboard')
-	logError(error)
 }
